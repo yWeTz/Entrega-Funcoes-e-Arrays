4fd60e46f4fd2f8fc82ed614b97d20cf40193ca6
Sidarta Kauã <odssy351@gmail.com>
2022-04-14 15:15:40 -0500

    atendendo aos criterios da rubrica

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 //O "Daniel", localizado na minha const atletas[2],
 // pode mudar de lugar com os outros atletas.
 
-const atletas = ["Rafael", "Manoel", "Daniel"];
 function positions(firstPlace, secondPlace, lastPlace) {
+  const atletas = [firstPlace, secondPlace, lastPlace];
   for (let i = 0; i < atletas.length; i++) {
     if ("Daniel" == atletas[2]) {
-      atletas.splice(1, 2, "Daniel", "Manoel");
-    } else if ("Daniel" == atletas[1]) {
-      atletas.splice(0, 3, "Daniel", "Rafael", "Manoel");
+      atletas.splice(1, 2, lastPlace, secondPlace);
+      return console.log(`1ª - Colocado ${atletas[0]}
+2ª - Colocado ${atletas[1]}
+3ª - Colocado ${atletas[2]}`);
     }
-
-    return console.log(`${firstPlace + " " + atletas[0]} 
-${secondPlace + " " + atletas[1]}
-${lastPlace + " " + atletas[2]}`);
+  }
+  if ("Daniel" == atletas[1]) {
+    atletas.splice(0, 3, lastPlace, firstPlace, secondPlace);
+    return console.log(`1ª - Colocado ${atletas[0]}
+2ª - Colocado ${atletas[1]}
+3ª - Colocado ${atletas[2]}`);
   }
 }
 
-positions("1ª - Colocado", "2ª - Colocado", "3ª - Colocado");
+positions("Rafael", "Manoel", "Daniel");
