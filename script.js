74fc8cc5802668e839566e156f1fe7739cc08412
Sidarta Kauã <odssy351@gmail.com>
2022-04-18 16:00:15 -0500

    consertando a logica

--- script.js
+++ script.js
@@ -3,20 +3,30 @@
 
 function positions(firstPlace, secondPlace, lastPlace) {
   const atletas = [firstPlace, secondPlace, lastPlace];
-  for (let i = 0; i < atletas.length; i++) {
-    if ("Daniel" == atletas[2]) {
-      atletas.splice(1, 2, lastPlace, secondPlace);
-      return console.log(`1ª - Colocado ${atletas[0]}
+  if ("Daniel" == atletas[2]) {
+    atletas.splice(1, 2, lastPlace, secondPlace);
+    return console.log(`1ª - Colocado ${atletas[0]}
 2ª - Colocado ${atletas[1]}
 3ª - Colocado ${atletas[2]}`);
-    }
   }
   if ("Daniel" == atletas[1]) {
-    atletas.splice(0, 3, lastPlace, firstPlace, secondPlace);
+    atletas.splice(0, 3, secondPlace, firstPlace, lastPlace);
+    return console.log(`1ª - Colocado ${atletas[0]}
+2ª - Colocado ${atletas[1]}
+3ª - Colocado ${atletas[2]}`);
+  }
+  if ("Daniel" == atletas[0]) {
+    return console.log(`1ª - Colocado ${atletas[0]}
+2ª - Colocado ${atletas[1]}
+3ª - Colocado ${atletas[2]}`);
+  }
+  if ("Daniel" == atletas[0]) {
     return console.log(`1ª - Colocado ${atletas[0]}
 2ª - Colocado ${atletas[1]}
 3ª - Colocado ${atletas[2]}`);
   }
 }
-
 positions("Rafael", "Manoel", "Daniel");
+positions("Rafael", "Daniel", "Manoel");
+positions("Daniel", "Rafael", "Manoel");
+positions("Daniel", "Manoel", "Rafael");
